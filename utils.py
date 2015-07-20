56c95f2399e5d91a2506fe6433d6d77c399d6361
lcoandrade <lcoandrade@gmail.com>
2015-07-20 17:02:31 -0300

    First test

--- utils.py
+++ utils.py
@@ -22,10 +22,32 @@
 """
 
 import subprocess
+import urllib2
+import sys
 
 class Utils:
     def __init__(self):
         pass
+    
+    def makeRequest(self):
+        osmUrl = 'http://10.67.198.233/teste.cgi'
+        postFile = {'msg':'FUNCIONA!!!!'}
+        req = urllib2.Request(url=osmUrl, data=postFile, headers={'Content-Type': 'application/xml'})
+        return req
+
+    def run(self):
+        req = self.makeRequest()
+
+        try:
+            response = urllib2.urlopen(req)
+        except urllib2.URLError, e:
+            self.signals.errorOccurred.emit('Error occurred: '+str(e.args) + '\nReason: '+str(e.reason))
+            return
+        except urllib2.HTTPError, e:
+            self.signals.errorOccurred.emit('Error occurred: '+str(e.code) + '\nReason: '+str(e.msg))
+            return
+        
+        ret = response.read()
 
     def runProcess(self, cmd_list):
         #command samples

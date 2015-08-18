559fe51a229764b163dc53a987b70467a48d6089
lcoandrade <lcoandrade@gmail.com>
2015-08-18 09:58:41 -0300

    New translation files...

---
+++ i18n/DsgManagementTools_pt.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="pt_BR" sourcelanguage="en_US">
+<context>
+    <name>DsgManagementTools</name>
+    <message>
+        <location filename="dsg_management_tools.py" line="171"/>
+        <source>&amp;DsgManagementTools</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools.py" line="161"/>
+        <source>DSG Management Tools</source>
+        <translation>Ferramentas de Gerenciamento da DSG</translation>
+    </message>
+</context>
+<context>
+    <name>DsgManagementToolsDialog</name>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="78"/>
+        <source>Select Database</source>
+        <translation>Selecione um Banco de Dados</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="290"/>
+        <source>Warning!</source>
+        <translation>Aviso!</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="233"/>
+        <source>Please, select a cluster to be removed.</source>
+        <translation>Por favor, selecione um cluster para ser removido.</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="277"/>
+        <source>Information!</source>
+        <translation>Informação!</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="185"/>
+        <source>Error while creating cluster:</source>
+        <translation>Erro durante criação de cluster:</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="203"/>
+        <source>Replication started successfully for cluster:</source>
+        <translation>Replicação iniciada com sucesso para o cluster:</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="205"/>
+        <source>Error while starting replication:</source>
+        <translation>Erro durante inicialização da replicação:</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="277"/>
+        <source>Replication stopped successfully for cluster:</source>
+        <translation>Replicação parada com sucesso para o cluster:</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="279"/>
+        <source>Error while stopping replication:</source>
+        <translation>Erro durante a parada da replicação:</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog.py" line="290"/>
+        <source>Error while checking for active replications:
+</source>
+        <translation>Erro durante a conferência de replicações ativas:</translation>
+    </message>
+</context>
+<context>
+    <name>DsgManagementToolsDialogBase</name>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="14"/>
+        <source>Dsg Management Tools</source>
+        <translation>Ferramentas de Gerenciamento da DSG</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="34"/>
+        <source>Configure Cluster</source>
+        <translation>Configurar Cluster</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="227"/>
+        <source>Server</source>
+        <translation>Servidor</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="235"/>
+        <source>Host/Database</source>
+        <translation>Host/Banco de Dados</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="65"/>
+        <source>Cluster</source>
+        <translation>Cluster</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="144"/>
+        <source>Client</source>
+        <translation>Cliente</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="107"/>
+        <source>Create Cluster</source>
+        <translation>Criar Cluster</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="167"/>
+        <source>Start Replication</source>
+        <translation>Iniciar Replicação</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="213"/>
+        <source>Remove Cluster</source>
+        <translation>Remover Cluster</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="251"/>
+        <source>Clusters</source>
+        <translation>Clusters</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="259"/>
+        <source>Stop Replication</source>
+        <translation>Parar Replicação</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="267"/>
+        <source>Running Daemons</source>
+        <translation>Replicações Ativas</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="273"/>
+        <source>Refresh Slon Daemons</source>
+        <translation>Atualizar Replicações Ativas</translation>
+    </message>
+    <message>
+        <location filename="dsg_management_tools_dialog_base.ui" line="281"/>
+        <source>Daemons</source>
+        <translation>Processos</translation>
+    </message>
+</context>
+</TS>

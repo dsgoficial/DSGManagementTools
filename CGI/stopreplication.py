43f1aefaaf0ea39096e45ba80463568a4d45db73
lcoandrade <lcoandrade@gmail.com>
2015-08-26 14:30:14 -0300

    Now the stop replication is keeping the logs

--- CGI/stopreplication.py
+++ CGI/stopreplication.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import subprocess
-import os
+import os, time
 # Import modules for CGI handling 
 import cgi, cgitb
 cgitb.enable()
@@ -18,15 +18,25 @@ def runCall(cmd):
     subprocess.call(cmd, shell=True)
     
 def storeRunningDaemons():
-    listdaemons = 'ps -aux |grep -E \'/usr/bin/slon.*'+separador+'\'|grep -v grep | awk \'{print \"/usr/bin/nohup \"$11\" \"$12\" \"$13\" \"$14\" \"$15\" \"$16\" &\"}\' > running_daemons.log'
+    listdaemons = 'ps -aux |grep -E \'/usr/bin/slon.*'+separador+'\'|grep -v grep | awk \'{print \"\"$8\" \"$11\" \"$12\" \"$13\" \"$14\" \"$15\" \"$16\"\"}\' > running_daemons.log'
     runCall(listdaemons)
     
     daemons = open('running_daemons.log', 'r')
     lines = daemons.readlines()
     daemons.close()
-    
+    newlines = []
+    for line in lines:
+        split = line.split(' ')
+        if split[0] == 'S':
+            if split[5] == 'host=10.67.198.228':
+                hostType = 'slave'
+            else:
+                hostType = 'master'
+            s = '/usr/bin/nohup '+line[2::].strip('\n')+' >> '+split[2]+'_'+hostType+'.log &\n'
+            newlines.append(s)
+                
     slon_restore = open('dsg_slon.sh', 'wb')
-    conteudo = ['#!/bin/bash\n'] + lines
+    conteudo = ['#!/bin/bash\n'] + newlines
     slon_restore.writelines(conteudo)
     slon_restore.close()
     
@@ -47,6 +57,7 @@ killpids = 'ps -aux |grep '+clustername+' | awk \'{print $2}\' >> pids.log'
 
 # Killing daemons
 killPIDs()
+time.sleep(2)
 
 # Updating running slon daemons
 storeRunningDaemons()

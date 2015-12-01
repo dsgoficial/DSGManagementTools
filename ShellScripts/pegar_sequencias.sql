ae3e5044e0b18c65908a1f98989db008b81b72ef
Luiz Andrade <lcoandrade@gmail.com>
2015-12-01 11:24:31 -0200

    Replicating sequences... This is important when changing machile roles (eg. changing a machine from master to slave)

---
+++ ShellScripts/pegar_sequencias.sql
@@ -0,0 +1,7 @@
+﻿SELECT S.relname FROM pg_class AS S
+            INNER JOIN pg_depend AS D ON S.oid = D.objid
+            INNER JOIN pg_class AS T ON D.refobjid = T.oid
+            INNER JOIN pg_attribute AS C ON D.refobjid = C.attrelid AND D.refobjsubid = C.attnum
+            INNER JOIN pg_namespace N ON N.oid = S.relnamespace
+            WHERE S.relkind = 'S' AND N.nspname = 'cb'
+            ORDER BY S.relname

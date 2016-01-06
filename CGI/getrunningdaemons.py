f67b2f3bc3057e238fef73ca8ec18fd0cdaa4584
Luiz Andrade <lcoandrade@gmail.com>
2016-01-06 15:49:34 -0200

    Updating getrunningdeamons.py

--- CGI/getrunningdaemons.py
+++ CGI/getrunningdaemons.py
@@ -78,7 +78,7 @@ def makeResponse(lines):
         
         success, ev_seqno, ev_timestamp = checkSync(line)
         if not success:
-            return
+            return ''
         if ev_seqno and ev_timestamp:
             response += ' (Último Sincronismo em: '+ev_timestamp+')'
         else:
